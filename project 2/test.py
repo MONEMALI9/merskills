d9e284c5e9d242dd6da11e37b8fc5b8db6dffae4
MONEMALI9 <monem921999@gmail.com>
2023-09-17 12:07:31 +0300

    .

---
+++ project 2/test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Load your data and print out a few lines. Perform operations to inspect data
+# types and look for instances of missing or possibly errant data.
+diabetes = pd.read_csv('data\diabetes.csv')
